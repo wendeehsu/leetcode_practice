5b0bd3f591c11c41cdf7a227ad9424b4ad58220c
wendeehsu <b05705046@ntu.edu.tw>
2022-09-13 10:39:24 -0500

    fix: coin change

--- Array/coinChange.py
+++ Array/coinChange.py
@@ -1,8 +1,8 @@
 coins = [186,419,83,408]
-amount = 6249
+amount = 6
 
 coins.sort()
-print(coins)
+maxAmount = 99999
 
 """
 idea:
@@ -10,3 +10,17 @@ idea:
   - number at index 0 = the min number of coins needed for amount 0
   - number at index n = the min number of coins needed for amount n
 """
+minAmountList = [maxAmount] * (amount+1)
+minAmountList[0] = 0
+
+for i in range(amount+1):
+    for coin in coins:
+        # index is in list range(0, amount+1)
+        if i - coin <= amount and i - coin >= 0:
+            if minAmountList[i-coin] != maxAmount:
+                minAmountList[i] = min(minAmountList[i], minAmountList[i-coin]+1)
+
+if minAmountList[amount] == maxAmount:
+    print(-1)
+else:
+    print(minAmountList[amount])
