ddcffe4ac808dc2e50c67ec56c24b14fd8d77d4d
wendeehsu <b05705046@ntu.edu.tw>
2022-09-19 23:53:39 -0500

    fix: top k frequent element

--- Heap/top_k_frequent_element.py
+++ Heap/top_k_frequent_element.py
@@ -1,38 +1,17 @@
-nums = [1]
+nums = [1,1,1,2,2,3]
 k = 2
 
-sortedList = []
-
+sortedList = {}
 def updateList(letter):
-    target_index = -1
-    for index, pair in enumerate(sortedList):
-        if pair[0] == letter:
-            pair[1] += 1
-            target_index = index
-            break
-    """
-    target_index:
-    -1: create pair
-    exist: pop
-    -> insert pair
-    """
-    target_pair = [letter, 1]
-    if target_index != -1:
-        target_pair = sortedList.pop(target_index)
-    
-    insert_index = -1
-    for index, pair in enumerate(sortedList):
-        if pair[1] <= target_pair[1]:
-            insert_index = index
-            break
-    if insert_index == -1:
-        sortedList.append(target_pair)
+    if letter in sortedList:
+        sortedList[letter] += 1
     else:
-        sortedList.insert(insert_index, target_pair)
+        sortedList[letter] = 1
 
 for i in nums:
     updateList(i)
 
+sortedList = sorted(sortedList.items(), key=lambda item: item[1], reverse=1)
 finalList = []
 for i in range(k):
     if i < len(sortedList):
