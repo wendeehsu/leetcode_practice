0685cf1f394075ec3bbec5e5109448ab23d52a9a
wendeehsu <b05705046@ntu.edu.tw>
2022-09-20 09:50:44 -0500

    fix: faster way of contains duplicate check

--- Array/contains_duplicate.py
+++ Array/contains_duplicate.py
@@ -1,2 +1,9 @@
 nums = [1,2,3,1]
-print(len(set(nums)) != len(nums))  
+
+hashTable = {}
+for i in nums:
+    if i in hashTable:
+        print(True)
+        break
+    else:
+        hashTable[i] = 1
