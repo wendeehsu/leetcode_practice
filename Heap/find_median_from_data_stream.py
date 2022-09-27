db06458462b625d04eae38193b81e44d11b0accd
wendeehsu <b05705046@ntu.edu.tw>
2022-09-27 11:01:20 -0500

    fix: use sort when finding median

--- Heap/find_median_from_data_stream.py
+++ Heap/find_median_from_data_stream.py
@@ -4,20 +4,10 @@ class MedianFinder:
         self.arr = []
 
     def addNum(self, num: int) -> None:
-        if len(self.arr) == 0:
-            self.arr = [num]
-        else:
-            insertIndex = -1
-            for index, number in enumerate(self.arr):
-                if number >= num:
-                    insertIndex = index
-                    break
-            if insertIndex == -1:
-                self.arr += [num]
-            else:
-                self.arr.insert(insertIndex, num)
+        self.arr += [num]
 
     def findMedian(self) -> float:
+        self.arr.sort()
         arr_length = len(self.arr)
         if arr_length % 2 == 0:
             return 0.5*(self.arr[arr_length//2] + self.arr[(arr_length//2)-1])
