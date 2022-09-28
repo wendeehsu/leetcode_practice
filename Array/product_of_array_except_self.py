ecf18a004c7be1fb1e79ca849a4eefff8b2d1a44
wendeehsu <b05705046@ntu.edu.tw>
2022-09-28 10:51:02 -0500

    fix: without devision

--- Array/product_of_array_except_self.py
+++ Array/product_of_array_except_self.py
@@ -1,15 +1,16 @@
 import math
 nums = [-1,1,0,-3,3]
+right = [1] * len(nums)
+left = [1] * len(nums)
 
-total = math.prod(nums)
-def getOtherProduct(index,num):
-    if num == 0:
-        return math.prod(nums[:index] + nums[index+1:])
-    else:
-        return total//num
+for i in range(1,len(nums)):
+    left[i] = left[i-1]*nums[i-1]
+
+for i in range(len(nums)-2,-1,-1):
+    right[i] = right[i+1]*nums[i+1]
 
 result = []
-for i, num in enumerate(nums):
-    result += [getOtherProduct(i,num)]
+for i in range(len(nums)):
+    result += [right[i]*left[i]]
 
 print(result)
