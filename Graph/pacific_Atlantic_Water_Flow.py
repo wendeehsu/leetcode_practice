c0cb4ee663d59f7c4d50dcdc687725492e5df143
wendeehsu <b05705046@ntu.edu.tw>
2022-09-29 16:50:39 -0500

    fix: water flow with bfs deque

--- Graph/pacific_Atlantic_Water_Flow.py
+++ Graph/pacific_Atlantic_Water_Flow.py
@@ -1,49 +1,43 @@
-
+from collections import deque
 heights = [[1,2,3],[8,9,4],[7,6,5]]
 
-def printMatrix(m):
-    for i in m:
-        print(i)
+directions = [(0,1),(1,0),(-1,0),(0,-1)]
+pacific = set()
+atlantic = set()
 
-def initMatrix():
-    m = len(heights[0])
-    n = len(heights)
-    pa_matrix = [[1]*m]
-    for row in range(1,n):
-        pa_matrix += [[1] + (m-1)*[0]]
-    
-    at_matrix = []
-    for row in range(n-1):
-        at_matrix += [(m-1)*[0]+[1]]
-    at_matrix += [[1]*m]
-    
-    return pa_matrix, at_matrix
-
-pa_matrix, at_matrix = initMatrix()
-
-# pacific
-for i in range(1,len(heights)):
-    for j in range(1,len(heights[0])):
-        if heights[i][j] >= heights[i-1][j]:
-            pa_matrix[i][j] = max(pa_matrix[i][j],pa_matrix[i-1][j])
-        if heights[i][j] >= heights[i][j-1]:
-            pa_matrix[i][j] = max(pa_matrix[i][j],pa_matrix[i][j-1])
-
-
-# atlantic
-for i in range(len(heights)-2,-1,-1):
-    for j in range(len(heights[0])-2,-1,-1):
-        if heights[i][j] >= heights[i+1][j]:
-            at_matrix[i][j] = max(at_matrix[i][j],at_matrix[i+1][j])
-        if heights[i][j] >= heights[i][j+1]:
-            at_matrix[i][j] = max(at_matrix[i][j],at_matrix[i][j+1])
-
-result = []
+# init reachable
+# row
 for i in range(len(heights)):
-    for j in range(len(heights[0])):
-        if at_matrix[i][j] == 1 and pa_matrix[i][j] == 1:
-            result += [[i,j]]
-# printMatrix(pa_matrix)
-# print("--")
-# printMatrix(at_matrix)
-print(result)
+    pacific.add((i,0))
+    atlantic.add((i,len(heights[0])-1))
+
+# column
+for j in range(len(heights[0])):
+    pacific.add((0,j))
+    atlantic.add((len(heights)-1,j))
+
+def bfs(ocean):
+    ocean = deque(ocean)
+    reachable = set()
+
+    while len(ocean) > 0:
+        node = ocean.popleft()
+        reachable.add(node)
+        for direction in directions:
+            x = node[0] + direction[0]
+            y = node[1] + direction[1]
+            
+            if x < 0 or x >= len(heights) \
+                or y < 0 or y >= len(heights[0]):
+                continue # this node is not valid
+            
+            if (x,y) in reachable:
+                continue # prevent TLE on re-computing
+            
+            if heights[node[0]][node[1]] <= heights[x][y]:
+                ocean.append((x,y))
+                reachable.add((x,y))
+
+    return reachable
+
+print(list(bfs(atlantic).intersection(bfs(pacific))))
