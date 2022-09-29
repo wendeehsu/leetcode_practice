6c19709a219d2c8024806b9bc4ff6ebc73c9c02b
wendeehsu <b05705046@ntu.edu.tw>
2022-09-28 19:33:55 -0500

    fix: create dp_matrix

--- Dynamic/longest_common_sequence.py
+++ Dynamic/longest_common_sequence.py
@@ -1,20 +1,24 @@
 text1 = "wendee"
 text2 = "edw"
 
-start2 = len(text2)
-dp = [0] * (len(text1) + 1)
+# create empty matrix
+dp_matrix = []
+for i in range(len(text2)+1):
+    dp_matrix += [[0] * (len(text1) + 1)]
+
+# print(dp_matrix)
 
 for i in range(len(text1)-1,-1,-1):
-    # print(i,text1[i])
-    hasFound = False
-    for j in range(len(text2),-1,-1):
-        if text1[i] == text2[j]:              
-            start2 = j
-            dp[i] = dp[i+1] + 1
-            hasFound = True
-            break
-    if hasFound == False:
-        dp[i] = dp[i+1]
+    for j in range(len(text2)-1,-1,-1):
+        """
+        Case 1: text1[i] == text2[j]: dp_matrix[i][j] = dp_matrix[i+1][j+1]+1
+        Case 2: text1[i] != text2[j]:
+                decide to take letter from text1 or text2
+                dp_matrix[i][j] = max(dp_matrix[i+1][j],dp_matrix[i][j+1])
+        """
+        if text1[i] == text2[j]:
+            dp_matrix[j][i] = dp_matrix[j+1][i+1]+1
+        else:
+            dp_matrix[j][i] = max(dp_matrix[j+1][i],dp_matrix[j][i+1])
 
-print(dp)
-print("result ->", dp[0])
+print(dp_matrix[0][0])
