bb74d2ea5ad9d3cb2b305aee4c13df491f031555
wendeehsu <b05705046@ntu.edu.tw>
2022-10-11 15:15:34 -0500

    fix: O(n)

--- Array/maximum_Subarray.py
+++ Array/maximum_Subarray.py
@@ -1,26 +1,16 @@
-nums = [-2,1,-3,4,-1,2,1,-5,4]
-# nums = [-4,-1,0]
+# nums = [-2,1,-3,4,-1,2,1,-5,4]
+# nums = [8,-19,5,-4,20]
+# nums = [5,4,-1,7,8]
+# nums = [1,-1,1]
+nums = [3,-3,2,-3]
 
 def maxSubArray(nums):
-    def getPositive(nums):
-        indexs = []
-        for i, num in enumerate(nums):
-            if num > 0:
-                indexs += [i]
-        return indexs
-
-    positiveIndexs = getPositive(nums)
-    if len(positiveIndexs) == 0:
-        return max(nums)
-    if len(positiveIndexs) == 1:
-        return nums[positiveIndexs[0]]
-    
-    startIndex = endIndex = positiveIndexs[0]
-    maxSum = 0
-    for i in range(len(positiveIndexs)):
-        for j in range(i,len(positiveIndexs)):
-            localSum = sum(nums[positiveIndexs[i]:positiveIndexs[j]+1])
-            maxSum = max(maxSum, localSum)
+    maxSum = nums[0]
+    currentArray = nums[0]
+    for i in range(1,len(nums)):
+        num = nums[i]
+        currentArray = max(num, currentArray + num)
+        maxSum = max(maxSum, currentArray)
     return maxSum
 
 print("maxSubArray -> ", maxSubArray(nums))
