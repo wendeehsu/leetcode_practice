49822e989ec0c5966c8c37f6d8886ef50c0f2de3
wendeehsu <b05705046@ntu.edu.tw>
2022-10-12 20:30:15 -0500

    feat: dfs

--- Graph/number_of_islands.py
+++ Graph/number_of_islands.py
@@ -12,23 +12,20 @@ def printGrid(g):
         print(i)
 
 def explore(x,y):
-    island = deque([[x,y]])
-    while len(island) > 0:
-        start = island.popleft()
-        grid[start[0]][start[1]] = '#'
-        for direction in [(0,1),(0,-1),(1,0),(-1,0)]:
-            new_x = start[0] + direction[0]
-            new_y = start[1] + direction[1]
+    grid[x][y] = '#'
+    for direction in [(0,1),(0,-1),(1,0),(-1,0)]:
+        new_x = x + direction[0]
+        new_y = y + direction[1]
 
-            if new_x < 0 or new_x >= len(grid):
-                continue
+        if new_x < 0 or new_x >= len(grid):
+            continue
 
-            if new_y < 0 or new_y >= len(grid[0]):
-                continue
-            
-            if grid[new_x][new_y] == '1':
-                grid[new_x][new_y] = '#'
-                island.append([new_x,new_y])
+        if new_y < 0 or new_y >= len(grid[0]):
+            continue
+        
+        if grid[new_x][new_y] == '1':
+            grid[new_x][new_y] = '#'
+            explore(new_x,new_y)
         
 numIsland = 0
 for i in range(len(grid)):
