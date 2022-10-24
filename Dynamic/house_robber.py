b830e5c4ce277e7495730eebed0e73dcb7a4d4a0
wendeehsu <b05705046@ntu.edu.tw>
2022-10-24 16:07:52 -0500

    fix: space complexity to O(1)

--- Dynamic/house_robber.py
+++ Dynamic/house_robber.py
@@ -1,5 +1,6 @@
 nums = [1,2,3,1]
 # nums = [2,7,9,3,1]
+# nums = [1,1]
 
 def startRob(nums):
     if len(nums) == 0:
@@ -7,13 +8,13 @@ def startRob(nums):
     if len(nums) == 1:
         return nums[0]
 
-    dp = [0] * (1+len(nums))
-    dp[1] = nums[0]
+    currentMax = lastMax = 0
+    for i in range(len(nums)):
+        tmp = currentMax
+        currentMax = max(lastMax + nums[i], currentMax)
+        lastMax = tmp
 
-    for i in range(2,len(dp)):
-        dp[i] = max(dp[i-1],dp[i-2]+nums[i-1])
-    print(nums)
-    print(dp)
-    return dp[-1]
+    # print(nums)
+    return currentMax
 
 print("rob result ->",startRob(nums))
