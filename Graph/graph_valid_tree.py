4ba8982933e8e12de122014e69029345ba6ad39f
wendeehsu <b05705046@ntu.edu.tw>
2022-11-03 16:09:00 -0500

    fix: improve

--- Graph/graph_valid_tree.py
+++ Graph/graph_valid_tree.py
@@ -1,22 +1,22 @@
 n = 5
 edges = [[0,1],[1,2],[2,3],[1,3],[1,4]]
 
-def dfs(visited, edgeDic, parent, priorList):
-    visited[parent] = 1
-    priorList += [parent]
-    for child in edgeDic[parent]:
-        if len(priorList) >=2 and child == priorList[-2]:
+def dfs(visited, edgeDic, current, parent):
+    visited[current] = 1
+    for child in edgeDic[current]:
+        if child == parent:
             continue
         if visited[child] == 1:
             return False
         if visited[child] == 0:
-            if dfs(visited,edgeDic,child, priorList) == False:
+            if dfs(visited,edgeDic,child, current) == False:
                 return False
-    visited[parent] = 2
-    priorList.pop()
     return True
 
 def traverse(n, edges):
+    if len(edges) != n-1:
+        return False
+        
     edgeDic = {}
     visited = [0] * n
     for i in range(n):
@@ -26,16 +26,16 @@ def traverse(n, edges):
         edgeDic[edge[0]] += [edge[1]]
         edgeDic[edge[1]] += [edge[0]]
     
-    counter = 0
-    for i in range(n):
-        if visited[i] == 0: # not visited
-            if counter > 0:
-                return False
+    # counter = 0
+    # for i in range(n):
+    #     if visited[i] == 0: # not visited
+    #         if counter > 0:
+    #             return False
 
-            if dfs(visited, edgeDic, i, []) == False:
-                return False
-            counter += 1
+    #         if dfs(visited, edgeDic, i, -1) == False:
+    #             return False
+    #         counter += 1
 
-    return True
+    return dfs(visited, edgeDic, 0, -1) and sum(visited) == len(visited)
 
 print(traverse(n, edges))
