03a21bec4201d2fe9ed9873a6158bd86da9431d7
wendeehsu <b05705046@ntu.edu.tw>
2022-12-01 15:54:03 -0600

    feat: add range

--- Tree/validate_binary_search_tree.py
+++ Tree/validate_binary_search_tree.py
@@ -1,33 +1,9 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def getMax(node):
-            if node.left == None and node.right == None:
-                return node.val
-            elif node.left == None and node.right != None:
-                return max(node.val, getMax(node.right))
-            elif node.left != None and node.right == None:
-                return max(node.val, getMax(node.left))
-            else:
-                return max(node.val, getMax(node.right), getMax(node.left))
-            
-        def getMin(node):
-            if node.left == None and node.right == None:
-                return node.val
-            elif node.left == None and node.right != None:
-                return min(node.val, getMin(node.right))
-            elif node.left != None and node.right == None:
-                return min(node.val, getMin(node.left))
-            else:
-                return min(node.val, getMin(node.right), getMin(node.left))
-            
-        def isTreeValid(node):
+        def isTreeValid(node, high=2**31+1, low=-2**31-1):
             if node == None:
                 return True
-            if node.right != None:
-                if node.val >= getMin(node.right):
-                    return False
-            if node.left != None:
-                if node.val <= getMax(node.left):
-                    return False
-            return  isTreeValid(node.left) and isTreeValid(node.right)
+            if node.val <= low or node.val >= high:
+                return False
+            return isTreeValid(node.right, high, node.val) and isTreeValid(node.left, node.val, low)
         return isTreeValid(root)
