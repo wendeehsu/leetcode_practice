98d400e4da1a24a6b1d233dcb96e521e09013235
wendeehsu <b05705046@ntu.edu.tw>
2023-12-07 17:17:47 -0600

    fix: add day3

--- christmas/day3_2.py
+++ christmas/day3_2.py
@@ -30,50 +30,64 @@ def updateNumber(num1, newNum):
 for i, line in enumerate(Lines):
     line = line.replace("\n","")
     for j in range(len(line)):
-        if line[j] == "*":
-            num1 = None
-            num2 = None
-            print(i, "j =", j)
-            if i - 1 >= 0: # up
-                if j < len(line)-1 and Lines[i-1][j+1].isnumeric(): # up right
-                    tmp = getNumber(j+1, Lines[i-1])
-                    num1, num2 = updateNumber(num1, tmp)
-                elif Lines[i-1][j].isnumeric():
-                    tmp = getNumber(j, Lines[i-1])
-                    num1, num2 = updateNumber(num1, tmp)
-                    
-                elif j > 0 and Lines[i-1][j-1].isnumeric():
-                    tmp = getNumber(j-1, Lines[i-1])
-                    num1, num2 = updateNumber(num1, tmp)
-                    
-            if i + 1 < len(Lines): # down
-                if j < len(line)-1 and Lines[i+1][j+1].isnumeric(): # up right
-                    tmp = getNumber(j+1, Lines[i+1])
-                    num1, num2 = updateNumber(num1, tmp)
-                    
-                elif Lines[i+1][j].isnumeric():
-                    tmp = getNumber(j,Lines[i+1])
-                    num1, num2 = updateNumber(num1, tmp)
-                    
-                elif j > 0 and Lines[i+1][j-1].isnumeric():
-                    tmp = getNumber(j-1, Lines[i+1])
-                    num1, num2 = updateNumber(num1, tmp)
-                    
-            if j > 0 and line[j-1].isnumeric():
-                tmp = getNumber(j-1,line)
+        if line[j] != "*":
+            continue
+        
+        num1 = None
+        num2 = None
+        print(i, "j =", j)
+        if i - 1 >= 0: # up
+            # has two number
+            if j < len(line)-1 and j > 0 and \
+                Lines[i-1][j+1].isnumeric() and \
+                Lines[i-1][j-1].isnumeric() and \
+                Lines[i-1][j].isnumeric() == False:
+                num1 = getNumber(j-1, Lines[i-1])
+                num2 = getNumber(j+1, Lines[i-1])
+                print("two on top!", num1, num2)
+                count += num1*num2
+                continue
+            # has one number
+            if j < len(line)-1 and Lines[i-1][j+1].isnumeric(): # up right
+                num1 = getNumber(j+1, Lines[i-1])
+            elif Lines[i-1][j].isnumeric():
+                num1 = getNumber(j, Lines[i-1])                
+            elif j > 0 and Lines[i-1][j-1].isnumeric():
+                num1 = getNumber(j-1, Lines[i-1])
+                
+        if i + 1 < len(Lines): # down
+            if j < len(line)-1 and j > 0 and \
+                Lines[i+1][j+1].isnumeric() and \
+                Lines[i+1][j-1].isnumeric() and \
+                Lines[i+1][j].isnumeric() == False:
+                num1 = getNumber(j-1, Lines[i+1])
+                num2 = getNumber(j+1, Lines[i+1])
+                print("two on lowww!", num1, num2)
+                count += num1*num2
+                continue
+
+            if j < len(line)-1 and Lines[i+1][j+1].isnumeric(): # up right
+                tmp = getNumber(j+1, Lines[i+1])
                 num1, num2 = updateNumber(num1, tmp)
-            if j < len(line)-1 and line[j+1].isnumeric():
-                tmp = getNumber(j+1,line)
+                
+            elif Lines[i+1][j].isnumeric():
+                tmp = getNumber(j,Lines[i+1])
                 num1, num2 = updateNumber(num1, tmp)
-            if num1 != None and num2 != None:
-                print(count, "->", num1, num2)
-                count += num1* num2
-                # print(count)
-            if num1 == None or num2 == None:
-                print("empty------",i,j)
-            # print(count)
-            # if i > 5:
-            #     break
+                
+            elif j > 0 and Lines[i+1][j-1].isnumeric():
+                tmp = getNumber(j-1, Lines[i+1])
+                num1, num2 = updateNumber(num1, tmp)
+                
+        if j > 0 and line[j-1].isnumeric():
+            tmp = getNumber(j-1,line)
+            num1, num2 = updateNumber(num1, tmp)
+        if j < len(line)-1 and line[j+1].isnumeric():
+            tmp = getNumber(j+1,line)
+            num1, num2 = updateNumber(num1, tmp)
+        if num1 != None and num2 != None:
+            print(count, "->", num1, num2)
+            count += num1* num2   
         
 
 print("count ->", count)
+# 66097989
