e90cf6199cbef053bee253870f1f9b20758ca414
wendeehsu <b05705046@ntu.edu.tw>
2023-12-09 23:52:16 -0600

    feat: day 9

---
+++ christmas/day9_2.py
@@ -0,0 +1,17 @@
+file1 = open('input.txt', 'r')
+Lines = file1.readlines()
+
+def predict(nums):
+    diff = list(map(lambda i: nums[i]-nums[i-1],list(range(1,len(nums)))))
+    if len(set(diff)) == 1:
+        return nums[0] - diff[0]
+    return nums[0] - predict(diff)
+
+ans = 0
+for i, line in enumerate(Lines):
+    nums = list(map(lambda x: int(x),line.replace("\n","").split()))
+    nex = predict(nums)
+    ans += nex
+    print(i, "->", nex, ans)
+
+print("ans ->", ans)
