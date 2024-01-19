c142b6681f98b8f23b7050396d4d3001b7c76fb9
wendeehsu <b05705046@ntu.edu.tw>
2024-01-19 15:41:56 -0600

    fix: improve time

--- Stack/validateStackSequences.py
+++ Stack/validateStackSequences.py
@@ -4,27 +4,14 @@ def validateStackSequences(pushed: List[int], popped: List[int]) -> bool:
     if len(popped) != len(pushed):
         return False   
 
-    while len(popped) > 0:
-        item = popped.pop(0)
-        print(item, popped)
-        try:
-            index = pushed.index(item)
-            pushIndex = index - 1
-            popIndex = 0
-            while pushIndex >= 0:
-                if popped[popIndex] not in pushed[:pushIndex+1]:
-                    popIndex += 1
-                else:
-                    if popped[popIndex] != pushed[pushIndex]:
-                        return False
-                    else:
-                        popIndex += 1
-                        pushIndex -= 1
-            pushed.pop(index)
-        except:
-            return False
-    return True
-
+    stack = []
+    pointer = 0
+    for item in pushed:
+        stack.append(item)
+        while len(stack) > 0 and stack[-1] == popped[pointer]:
+            stack.pop(-1)
+            pointer += 1
+    return pointer == len(popped)
 
 pushed = [1,2,3,4,5]
 popped = [4,5,3,2,1]
